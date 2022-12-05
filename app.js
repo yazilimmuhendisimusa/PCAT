fe910ede9ec93c361fe6a1152913caae8f4ac8fd
musa topal <nodejsmusa@gmail.com>
2022-12-05 11:17:47 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const fn = function () {
-  console.log('Function Working');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
 };
 
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
 
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
