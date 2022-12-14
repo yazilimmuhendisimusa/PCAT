0ca36144d32f2670cab5c9ca9d6610a46045de19
musa topal <nodejsmusa@gmail.com>
2022-12-14 09:38:41 +0300

    4.bölüm 10.ödev Clean Project

--- app.js
+++ app.js
@@ -1,28 +1,73 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware Log 1');
-  next();
-};
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware Log 2');
-  next();
-};
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next();
+// };
+
+// const myLogger2 = (req, res, next) => {
+//   console.log('Middleware Log 2');
+//   next();
+// };
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+// app.use(myLogger);
+// app.use(myLogger2);
+
+
+//ROUTES
+app.get('/', async(req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  });
+});
+
+app.get('/about', (req, res) => {  
+  res.render('about');
+});
+
+app.get('/photos/:id', async(req, res) => {  
+  //console.log(req.params.id)
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo',{
+    photo
+  })
+});
+
+app.get('/add', (req, res) => {
+ 
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/')
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
+
